5891292839cc8544a32d0129c5c23041587b3e78
Anahi22k <kimberlyagaldamez06@gmail.com>
2026-03-05 16:42:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function showQuote(){
+
+const quote = document.getElementById("quote");
+
+quote.innerHTML = `
+"Queer African lives are not new.
+What is new is the political language used to describe them."
+`;
+
+}
